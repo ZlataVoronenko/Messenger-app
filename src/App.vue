cfe69160a1e802a3673c8c2feceb930e780f2131
Zlata Voronenko <116277114+ZlataVoronenko@users.noreply.github.com>
2022-12-18 23:30:05 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <v-main-wrapper />
+  </div>
+</template>
+
+<script>
+  import vMainWrapper from './components/v-main-wrapper'
+export default {
+  name: 'App',
+  components: {
+    vMainWrapper
+  }
+}
+</script>
